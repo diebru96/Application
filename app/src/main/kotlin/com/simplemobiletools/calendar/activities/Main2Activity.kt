f3d5333ff6629ffb7ba380d1dc8ec4589c22b9b0
Diego <s223875@studenti.polito.it>
2018-05-27 16:11:29 +0200

    Song List added

--- app/src/main/kotlin/com/simplemobiletools/calendar/activities/Main2Activity.kt
+++ app/src/main/kotlin/com/simplemobiletools/calendar/activities/Main2Activity.kt
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import com.simplemobiletools.calendar.R
+import com.simplemobiletools.calendar.debug.MusicActivity
 
 class Main2Activity : AppCompatActivity() {
 
@@ -16,5 +17,10 @@ class Main2Activity : AppCompatActivity() {
     fun openCalendar(v:View) {
         val intent = Intent(application, MainActivity::class.java)
         startActivity(intent)
+    }
+
+    fun openMusic(v:View) {
+        val intent2 = Intent(application, MusicActivity::class.java)
+        startActivity(intent2)
     }
 }
