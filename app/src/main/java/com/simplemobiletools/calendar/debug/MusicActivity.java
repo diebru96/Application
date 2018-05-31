f665e8fabd2db3708ed46b674fc8071ed5399533
Diego <s223875@studenti.polito.it>
2018-05-31 10:20:48 +0200

    Song List added

--- app/src/main/java/com/simplemobiletools/calendar/debug/MusicActivity.java
+++ app/src/main/java/com/simplemobiletools/calendar/debug/MusicActivity.java
@@ -12,20 +12,34 @@ import android.net.Uri;
 import android.provider.MediaStore;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
+
+import static android.widget.Toast.LENGTH_LONG;
 
 public class MusicActivity extends AppCompatActivity {
 
     private static final int MY_PERMISSION_REQUEST=1;
+    int serverResponseCode;
     ArrayList<String> arrayList;
     ListView listView;
     ArrayAdapter<String> adapter;
+    ArrayList<String> positions;
+    ArrayList<String> names;
+    private String basename="http://.";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -50,6 +64,8 @@ public class MusicActivity extends AppCompatActivity {
     public void doStuff() {
         listView= (ListView) findViewById(R.id.ListView);
         arrayList=new ArrayList<>();
+        names= new ArrayList<>();
+        positions=new ArrayList<>();
         getMusic();
         adapter=new ArrayAdapter<>(this,android.R.layout.simple_list_item_1, arrayList);
         listView.setAdapter(adapter);
@@ -57,6 +73,11 @@ public class MusicActivity extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 //open
+                int position=adapterView.getPositionForView(view);
+               String path=positions.get(position);
+                String name=names.get(position);
+                // Toast.makeText(getBaseContext(),name, Toast.LENGTH_SHORT ).show();
+                uploadFile(path,basename+"/api/v1.0/music", name);
             }
         });
     }
@@ -70,11 +91,15 @@ public class MusicActivity extends AppCompatActivity {
         {
             int songTitle=songCursor.getColumnIndex(MediaStore.Audio.Media.TITLE);
             int songArtist=songCursor.getColumnIndex(MediaStore.Audio.Media.ARTIST);
+            int songLocation=songCursor.getColumnIndex(MediaStore.Audio.Media.DATA);
 
             do{
                 String currentTitle=songCursor.getString(songTitle);
                 String currentArtist=songCursor.getString(songArtist);
+                String currentLocation=songCursor.getString(songLocation);
                 arrayList.add(currentTitle + "\n" + currentArtist);
+                positions.add(currentLocation);
+                names.add(currentTitle);
             }while(songCursor.moveToNext());
         }
     }
@@ -101,6 +126,147 @@ public class MusicActivity extends AppCompatActivity {
                 }
             }
         }
+    }
+
+    public int uploadFile(String sourceFileUri, String uploadFilePath , final String uploadFileName) {
+        String fileName = sourceFileUri;
+
+        HttpURLConnection conn = null;
+        DataOutputStream dos = null;
+        String lineEnd = "\r\n";
+        String twoHyphens = "--";
+        String boundary = "*****";
+        int bytesRead, bytesAvailable, bufferSize;
+        byte[] buffer;
+        int maxBufferSize = 1 * 1024 * 1024;
+        File sourceFile = new File(sourceFileUri);
+
+        if (!sourceFile.isFile()) {
+
+
+
+            Log.e("uploadFile", "Source File not exist :"
+                    +uploadFilePath + "" + uploadFileName);
+
+            runOnUiThread(new Runnable() {
+                public void run() {
+                    //messageText.setText("Source File not exist ");
+                }
+            });
+
+            return 0;
+
+        }
+        else
+        {
+            try {
+
+                // open a URL connection to the Servlet
+                FileInputStream fileInputStream = new FileInputStream(sourceFile);
+                URL url = new URL(uploadFilePath);
+
+                // Open a HTTP  connection to  the URL
+                conn = (HttpURLConnection) url.openConnection();
+                conn.setDoInput(true); // Allow Inputs
+                conn.setDoOutput(true); // Allow Outputs
+                conn.setUseCaches(false); // Don't use a Cached Copy
+                conn.setRequestMethod("POST");
+                conn.setRequestProperty("Connection", "Keep-Alive");
+                conn.setRequestProperty("ENCTYPE", "multipart/form-data");
+                conn.setRequestProperty("Content-Type", "multipart/form-data;boundary=" + boundary);
+                conn.setRequestProperty("uploaded_file", fileName);
+
+                dos = new DataOutputStream(conn.getOutputStream());
+
+                dos.writeBytes(twoHyphens + boundary + lineEnd);
+                dos.writeBytes("Content-Disposition: form-data; name= "+ uploadFileName + ";filename="
+                        + fileName );
+
+                dos.writeBytes(lineEnd);
+
+                // create a buffer of  maximum size
+                bytesAvailable = fileInputStream.available();
+
+                bufferSize = Math.min(bytesAvailable, maxBufferSize);
+                buffer = new byte[bufferSize];
+
+                // read file and write it into form...
+                bytesRead = fileInputStream.read(buffer, 0, bufferSize);
+
+                while (bytesRead > 0) {
+
+                    dos.write(buffer, 0, bufferSize);
+                    bytesAvailable = fileInputStream.available();
+                    bufferSize = Math.min(bytesAvailable, maxBufferSize);
+                    bytesRead = fileInputStream.read(buffer, 0, bufferSize);
+
+                }
+
+                // send multipart form data necesssary after file data...
+                dos.writeBytes(lineEnd);
+                dos.writeBytes(twoHyphens + boundary + twoHyphens + lineEnd);
+
+                // Responses from the server (code and message)
+                serverResponseCode = conn.getResponseCode();
+                String serverResponseMessage = conn.getResponseMessage();
+
+                Log.i("uploadFile", "HTTP Response is : "
+                        + serverResponseMessage + ": " + serverResponseCode);
+
+                if(serverResponseCode == 200){
+
+                    runOnUiThread(new Runnable() {
+                        public void run() {
+
+                            String msg = "File Upload Completed.\n\n See uploaded file here : \n\n"
+                                    +" http://www.androidexample.com/media/uploads/"
+                                    +uploadFileName;
+
+                            //messageText.setText(msg);
+                            Toast.makeText(getApplicationContext(), "File Upload Complete.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }
+
+                //close the streams //
+                fileInputStream.close();
+                dos.flush();
+                dos.close();
+
+            } catch (MalformedURLException ex) {
+
+                //dialog.dismiss();
+                ex.printStackTrace();
+
+                runOnUiThread(new Runnable() {
+                    public void run() {
+                        //    messageText.setText("MalformedURLException Exception : check script url.");
+                        Toast.makeText(getApplicationContext(), "MalformedURLException",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+                Log.e("Upload file to server", "error: " + ex.getMessage(), ex);
+            } catch (Exception e) {
+
+                //dialog.dismiss();
+                e.printStackTrace();
+
+                runOnUiThread(new Runnable() {
+                    public void run() {
+                        // messageText.setText("Got Exception : see logcat ");
+                        Toast.makeText(getApplicationContext(), "Got Exception : see logcat ",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                });
+                Log.e("Upload Exception", "Exception : "
+                        + e.getMessage(), e);
+            }
+            // dialog.dismiss();
+            return serverResponseCode;
+
+        } // End else block
     }
 }
 
